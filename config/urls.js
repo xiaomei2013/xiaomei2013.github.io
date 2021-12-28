dc2e59dde7586a454f9912d88646cc76a360795b
xufan <qifuren1985@gmail.com>
2021-12-28 15:19:43 +0800

    xm

---
+++ config/urls.js
@@ -0,0 +1,9 @@
+var iphoneUrl="https://itunes.apple.com/us/app/xiao-mei-sheng-huo/id894465362?l=zh&ls=1&mt=8";
+var androidUrl="http://cdn.xiaomei.com/app/xmlife/xmlife_2.5.168.apk";
+var httpUrl="service.xiaomei.com";
+var imageRes="http://res.xiaomei.com/";
+var instRes="http://instimage.xiaomei.com/";
+var shopperAndroidUrl="http://cdn.xiaomei.com/app/xmlife/supershoper_2.4.55.apk";
+var wxConfig = {
+    appId: 'wxdcbe0affffba53e0'
+};
